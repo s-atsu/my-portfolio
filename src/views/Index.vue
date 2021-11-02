627c6ebdeebfd42df8fe3038d5b7d07b5cc8e278
Atsuhiro Suzuki <s.atsuhiro.0522@gmail.com>
2021-11-03 06:21:45 +0900

    サービス一覧を実装

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,16 +1,20 @@
 <template>
   <main class="p-index">
     <Mv />
+
+    <Service />
   </main>
 </template>
 
 <script>
 import Mv from "../components/pages/top/Mv.vue";
+import Service from "../components/pages/top/Service.vue";
 
 export default {
   name: "Index",
   components: {
     Mv,
+    Service,
   },
 };
 </script>

e3dd974ae58f87aeb109119588b80ba29307b7d1
Atsuhiro Suzuki <s.atsuhiro.0522@gmail.com>
2021-11-03 06:29:17 +0900

    Aboutを実装

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -3,18 +3,33 @@
     <Mv />
 
     <Service />
+
+    <About />
   </main>
 </template>
 
 <script>
 import Mv from "../components/pages/top/Mv.vue";
 import Service from "../components/pages/top/Service.vue";
+import About from "../components/pages/top/About.vue";
 
 export default {
   name: "Index",
   components: {
     Mv,
     Service,
+    About,
   },
 };
 </script>
+
+<style lang="scss">
+.p-index__title {
+  font-size: 30px;
+  font-weight: bold;
+}
+.p-index__subtitle {
+  font-size: 14px;
+  display: block;
+}
+</style>

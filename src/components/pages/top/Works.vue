1666a9b16476bb44bd1d2f5276b5a46978d89883
Atsuhiro Suzuki <61732406+s-atsu@users.noreply.github.com>
2021-11-03 08:09:15 +0900

    worksを実装 (#7)

---
+++ src/components/pages/top/Works.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="p-index__works l-section">
+    <div class="l-container">
+      <h2 class="p-index__title">
+        WORKS
+        <span class="p-index__subtitle">過去の実績</span>
+      </h2>
+
+      <carousel :items-to-show="3">
+        <slide v-for="work in works" :key="work">
+          <WorksCard :works="work" />
+        </slide>
+      </carousel>
+    </div>
+  </section>
+</template>
+
+<script>
+import "vue3-carousel/dist/carousel.css";
+import { Carousel, Slide } from "vue3-carousel";
+import WorksCard from "../../parts/WorksCard.vue";
+
+export default {
+  components: {
+    Carousel,
+    Slide,
+    WorksCard,
+  },
+  data() {
+    return {
+      works: [
+        {
+          title: "Sample",
+          description: "Worksのサンプルです",
+          image: "/images/380x450.png",
+        },
+        {
+          title: "Sample",
+          description: "Worksのサンプルです",
+          image: "/images/380x450.png",
+        },
+        {
+          title: "Sample",
+          description: "Worksのサンプルです",
+          image: "/images/380x450.png",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/parts/WorksCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="c-works__card"
+    :style="{ backgroundImage: `url('${works.image}')` }"
+  >
+    <div class="c-works__content">
+      <p class="c-works__title">{{ works.title }}</p>
+      <p class="c-works__description">{{ works.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    works: [],
+  },
+};
+</script>
+
+<style lang="scss">
+.c-works__card {
+  width: calc(100% - 20px);
+  background: #f5f5f5;
+  background-size: 100%;
+  padding-top: 100%;
+}
+</style>
